f0a409f1b2ec23ddb6e76e84b1b11fee1309d6f4
andrroy <andreas@royrvik.org>
2015-03-31 17:58:39 +0200

    Articles are now shown in front end

--- client/flytoget/controllers/newsFeedCtrl.js
+++ client/flytoget/controllers/newsFeedCtrl.js
@@ -1,4 +1,5 @@
 angular.module('trickle-webapp').controller("NewsFeedCtrl",['$scope', '$meteor',
     function($scope, $meteor){
-
+        $scope.newsEntries = $meteor.collection(NewsEntries);
+        console.log($scope.newsEntries);
     }]);
