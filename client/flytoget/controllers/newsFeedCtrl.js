65ee2b4d1c3093f6b770fb6fba79a68bfe86ab83
andrroy <andreas@royrvik.org>
2015-03-31 19:09:09 +0200

    Removed logging, restored routes to their original (non-working) state

--- client/flytoget/controllers/newsFeedCtrl.js
+++ client/flytoget/controllers/newsFeedCtrl.js
@@ -1,5 +1,4 @@
 angular.module('trickle-webapp').controller("NewsFeedCtrl",['$scope', '$meteor',
     function($scope, $meteor){
         $scope.newsEntries = $meteor.collection(NewsEntries);
-        console.log($scope.newsEntries);
     }]);
