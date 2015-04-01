df87ecbb6ef81577a79e91bde45899d5a845db4b
andrroy <andreas@royrvik.org>
2015-04-02 00:04:13 +0200

    Webcam feed now working, refreshes every minute

--- client/flytoget/controllers/practicalCtrl.js
+++ client/flytoget/controllers/practicalCtrl.js
@@ -1,6 +1,15 @@
 angular.module('trickle-webapp').controller("practicalCtrl",['$scope', '$meteor',
     function($scope, $meteor){
 
-        console.log("works");
+        $scope.webcamFeed = "https://tjenester.avinor.no/internalservice-1.0/webcams/OSL/0";
+
+        var refreshWebcamFeed = function() {
+            var webcamImage = document.getElementsByClassName("camfeed")[0];
+            var d = new Date();
+            webcamImage.setAttribute("src", $scope.webcamFeed+ "?" + d.getTime());
+        };
+
+        // Refresh webcam feed every minute
+        setInterval(refreshWebcamFeed, 60000);
 
     }]);
