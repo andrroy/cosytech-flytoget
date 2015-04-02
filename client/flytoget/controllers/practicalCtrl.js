e4bc3798742bacd42a664aa2dc596498d2fbc013
andrroy <andreas@royrvik.org>
2015-04-02 13:17:54 +0200

    Started working on queue info

--- client/flytoget/controllers/practicalCtrl.js
+++ client/flytoget/controllers/practicalCtrl.js
@@ -1,5 +1,5 @@
-angular.module('trickle-webapp').controller("practicalCtrl",['$scope', '$meteor',
-    function($scope, $meteor){
+angular.module('trickle-webapp').controller("practicalCtrl",['$scope', '$meteor', '$http',
+    function($scope, $meteor, $http){
 
         $scope.webcamFeed = "https://tjenester.avinor.no/internalservice-1.0/webcams/OSL/0";
 
@@ -11,5 +11,19 @@ angular.module('trickle-webapp').controller("practicalCtrl",['$scope', '$meteor'
 
         // Refresh webcam feed every minute
         setInterval(refreshWebcamFeed, 60000);
+
+        var getQueueTime = function() {
+
+
+        }
+
+        $http.get("https://avinor.no/Api/QueueTime/OSL?language=no&_=1425427077776")
+            .success(function(res) {
+                console.log(res);
+                $scope.queueTime = JSON.parse(res).QueueTime.replace ( /[^\d.]/g, '' );
+            })
+            .error(function(err) {
+                console.log("QueueTime errro");
+            });
 
     }]);
