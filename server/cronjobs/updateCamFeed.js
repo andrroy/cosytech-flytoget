75f92d5dfc416a836aa77da7948ef3dde70a1e16
andrroy <andreas@royrvik.org>
2015-04-13 00:34:34 +0200

    Camfeed is now serverside

--- server/cronjobs/updateCamFeed.js
+++ server/cronjobs/updateCamFeed.js
@@ -1,13 +1,20 @@
 var fs = Meteor.npmRequire('fs');
 var https = Meteor.npmRequire('https');
-var path = Meteor.npmRequire('path');
 
 var url = "https://tjenester.avinor.no/internalservice-1.0/webcams/OSL/0";
 
-var file = fs.createWriteStream("/tmp/test.jpeg");
-var request = https.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function () {
-        file.close();
-    })
-});
+var basePath = process.env.PWD;
+
+updateCamfeed = function() {
+    var file = fs.createWriteStream(basePath + "/public/img/camfeed.jpeg");
+
+    var request = https.get(url, function(response) {
+        response.pipe(file);
+        file.on('finish', function () {
+            file.close();
+        })
+    });
+};
+
+// Update feed every 30 seconds
+Meteor.setInterval(updateCamfeed, 30000);
