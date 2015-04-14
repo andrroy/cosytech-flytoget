2882e491077b4852c5d6f654870ed2193facaed2
andrroy <andreas@royrvik.org>
2015-04-14 16:19:11 +0200

    Shameless hack to add new sexy icons

--- server/cronjobs/getWeather.js
+++ server/cronjobs/getWeather.js
@@ -1,7 +1,26 @@
 var url = "http://api.openweathermap.org/data/2.5/weather?lat=60.1952777778&lon=11.0994444444";
 
 var temp = 5;
-var icon = "01d.png";
+var icon = "cloudy.png";
+
+var iconHack = {
+    "01d": "sunny.png",
+    "01n": "sunny.png",
+    "02d": "sun_cloudy.png",
+    "02n": "sun_cloudy.png",
+    "03d": "cloudy.png",
+    "03n": "cloudy.png",
+    "04d": "cloudy.png",
+    "04n": "cloudy.png",
+    "09d": "rain.png",
+    "09n": "rain.png",
+    "10d": "rain.png",
+    "10n": "rain.png",
+    "11d": "rain.png",
+    "11n": "rain.png",
+    "13d": "more_snow.png",
+    "13n": "more_snow.png"
+};
 
 
 getTemp = function() {
@@ -18,7 +37,11 @@ updateWeather = function() {
     var response = JSON.parse(result.content);
 
     temp = (parseFloat(response.main.temp) - 273.15).toFixed(1);
-    icon = response.weather[0].icon + ".png";
+
+    icon = iconHack[response.weather[0].icon];
+    // Default to cloudy if no matching icon found
+    if(!icon)
+        icon = "cloudy.png";
 };
 
 // Is triggered every minute
